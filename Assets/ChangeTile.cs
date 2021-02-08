7fa2720216c6dafeb2efbfb30bec15bd20975d02
HEADSHOT739 <51834898+HEADSHOT739@users.noreply.github.com>
2021-02-08 11:28:10 -0500

    Permet de changer la tile

---
+++ Assets/ChangeTile.cs
@@ -0,0 +1,50 @@
+﻿/// Fonction qui permet de mettre toutes les tiles d'une couleurs différente
+/// 
+
+
+
+
+
+
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeTile : MonoBehaviour
+{
+    // Start is called before the first frame update
+   
+    private int NbTiles = 49;
+    
+    public MeshRenderer[] ListTiles;
+    [SerializeField]
+    public Material BlackMaterial;
+    [SerializeField]
+    public Material WhiteMaterial;
+
+    TileComponents textureBleu;
+    void Start()
+    {
+       
+        ListTiles = GetComponentsInChildren<MeshRenderer>();
+     
+      
+        foreach (MeshRenderer Floor in ListTiles)
+        {
+            if (Floor.material != WhiteMaterial &&
+                Floor.material != BlackMaterial)
+            {
+                
+                  Floor.material = (Random.value < 0.5)? WhiteMaterial : BlackMaterial;
+            }
+
+
+        } 
+
+
+
+    }
+   
+}
