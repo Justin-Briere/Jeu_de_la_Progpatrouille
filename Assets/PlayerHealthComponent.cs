16de57f9c5a446dea7534f3fab6ae13a3ef2f760
HEADSHOT739 <51834898+HEADSHOT739@users.noreply.github.com>
2021-02-08 11:28:57 -0500

    la vie du player/ permet de le tué

---
+++ Assets/PlayerHealthComponent.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealthComponent : MonoBehaviour
+{
+    GestionPlayer player;
+
+    [SerializeField]
+    private int maxHealthPoints = 5;
+
+    private int currentHealth;
+    EnemySpawnerComponent test;
+    EnemySpawnerComponent ObjectToClone;
+    private void Start()
+    {
+        ObjectToClone = GetComponentInParent<EnemySpawnerComponent>();
+        currentHealth = maxHealthPoints;
+    }
+
+    private void Update()
+    {
+        if (currentHealth <= 0)
+            DestroyObject();
+    }
+
+    public void DestroyObject()
+    {
+        //player.NewSpawn();
+
+        currentHealth = maxHealthPoints;
+        //  test = new EnemySpawnerComponent();
+        ObjectToClone.Spawn();
+        Destroy(gameObject);
+      
+
+
+    }
+    private void OnMouseDown()
+    {
+        currentHealth--;
+        Debug.Log(currentHealth);
+    }
+}
+
