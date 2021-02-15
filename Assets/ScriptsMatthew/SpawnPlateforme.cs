b03e929098d32e0cd10aae74283d404cb670041a
MatthewVerret <201973658@edu.clg.qc.ca>
2021-02-15 11:18:10 -0500

    SpawnPlateformyyyy
    Avec trigger, mais tout casser

--- Assets/ScriptsMatthew/SpawnPlateforme.cs
+++ Assets/ScriptsMatthew/SpawnPlateforme.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class SpawnPlateforme : MonoBehaviour
 {
+    public int current = 1;
     //Position de la plateforme de base
     public Transform SpawnPoint;
 
     //Objet à faire spawner
     public GameObject PlateForm;
-    
+
     // Lorsqu'il est trigger, les plate forme commence à spawn
-    private void OnTriggerEnter(Collider other)
+    public void SpawnPF(Transform spawnP)
     {
-        Instantiate(PlateForm, SpawnPoint.position, SpawnPoint.rotation);
+        Instantiate(PlateForm, spawnP.position, spawnP.rotation);
+        current = current + 1;
+    }
+
+    // Permet d'activer la première plateforme
+    private void OnMouseDown()
+    {
+         Instantiate(PlateForm, SpawnPoint.position, SpawnPoint.rotation);
     }
 }
---
+++ Assets/TriggerPlateForm.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerPlateForm : MonoBehaviour
+{
+    SpawnPlateforme TriggerSpawn = new SpawnPlateforme();
+
+    [SerializeField]
+    Transform [] spawnPositions = new Transform[3];
+
+    private void OnColliderEnter(Collider other)
+    {
+        TriggerSpawn.SpawnPF(spawnPositions[TriggerSpawn.current]);
+    }
+}
