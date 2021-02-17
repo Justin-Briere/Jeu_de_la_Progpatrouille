84caef6c341e6c58e2cac900225af72bfd9bf687
HEADSHOT739 <51834898+HEADSHOT739@users.noreply.github.com>
2021-02-16 19:41:39 -0500

    fIn de ma scene

---
+++ Assets/script Justin/CamScriptComponents.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamScriptComponents : MonoBehaviour
+{
+
+    public Camera cam1;
+    public Camera cam2;
+
+     
+    void Start()
+    {
+        cam1.enabled = true;
+        cam2.enabled = false;
+    }
+
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            cam1.enabled = !cam1.enabled;
+            cam2.enabled = !cam2.enabled;
+        }
+    }
+}
