6e5fe3a6a46d2f4a18d7905fabce1bf665c517ca
MatthewVerret <201973658@edu.clg.qc.ca>
2021-02-17 09:41:13 -0500

    PlateformChangesVitesse

--- Assets/ScriptsMatthew/PlateformMouvementComponent.cs
+++ Assets/ScriptsMatthew/PlateformMouvementComponent.cs
@@ -7,18 +7,39 @@ public class PlateformMouvementComponent : MonoBehaviour
     [SerializeField]
     private float speed = 1.5f;
 
+    private bool isMoving;
+
     private Vector3 vector = new Vector3(0,0,-1);
-    MovementComponent Follow;
+
+    //MovementComponent Follow;
+
     void Update()
     {
-        transform.Translate(vector * speed * Time.deltaTime);
+        if(isMoving)
+            transform.position += (vector * Time.deltaTime);
     }
-    private void OnCollisionEnter()
+    private void OnCollisionEnter(Collision collision)
     {
-        Debug.Log("ici");
-        var text = GameObject.FindGameObjectWithTag("Player");
-        text.transform.Translate(vector * speed * Time.deltaTime);
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            isMoving = true;
+            collision.collider.transform.SetParent(transform);
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+            collision.collider.transform.SetParent(null);
+    }
+    /*private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+            collision.collider.transform.SetParent(null);
+
+        ///var text = GameObject.FindGameObjectWithTag("Player");
+        //text.transform.Translate(vector * speed * Time.deltaTime);
         
        // Follow.Move(vector) = transform.Translate(vector * (speed * Time.deltaTime)); 
     }
+    */
 }
