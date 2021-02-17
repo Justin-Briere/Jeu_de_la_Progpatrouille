13987ad8e57a723a9038ecf76ada6cf25664848d
HEADSHOT739 <51834898+HEADSHOT739@users.noreply.github.com>
2021-02-17 08:44:50 -0500

    Fix de quelque box collider

--- Assets/TestJump101.cs
+++ Assets/TestJump101.cs
@@ -6,18 +6,22 @@ public class TestJump101 : MonoBehaviour
 {
     public int forceConst = 50;
     private Rigidbody selfRigidbody;
+    private Rigidbody Floor;
+    float dist;
 
     void Start()
     {
         selfRigidbody = GetComponent<Rigidbody>();
     }
 
-    void Update()
+    public void Jump()   
     {
-        if (Input.GetKeyUp(KeyCode.Space))
-        {
-
+        Debug.Log("Distance to other: " );
+       
+             dist = Vector3.Distance(selfRigidbody.gameObject.transform.position, Floor.gameObject.transform.position);
+           
             selfRigidbody.AddForce(0, forceConst, 0, ForceMode.Impulse);
-        }
+            selfRigidbody.AddForce(0, -9.81f, 0, ForceMode.Impulse);
+         Debug.Log ("Distance to other: " + dist);
     }
 }
