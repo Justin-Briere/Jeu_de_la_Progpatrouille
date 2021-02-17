b236e72a481e03accc85ca876bbd89dcf91f7221
WillMart91 <WillMartin91@outlook.com>
2021-02-17 11:03:10 -0500

    Jump + movement

---
+++ Assets/ScriptsWill/JumpAndMovement.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpAndMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 10f;
+
+    [SerializeField]
+    private float jumpForce = 350.0f;
+
+    [SerializeField]
+    private float divisionOfSpeedInAir = 5;
+
+    private bool OnGround = false;
+
+    private float cst = 1;
+
+    private Rigidbody player;
+
+    private Vector3 direction;
+
+    static private Vector3[] directions = new[]
+    {
+        new Vector3(0,0,1),
+        new Vector3(-1,0,0),
+        new Vector3(0,0,-1),
+        new Vector3(1,0,0),
+    };
+
+    void Start()
+    {
+        player = GetComponent<Rigidbody >();
+    }
+
+    void Update()
+    {
+        if(OnGround)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))            
+                MakeJump();          
+        }
+        else
+        {
+            cst = divisionOfSpeedInAir;
+        }
+
+        if (direction != Vector3.zero)
+            Move(direction.normalized);
+
+        direction = new Vector3();
+    }
+    public void OnCollisionEnter(Collision collision) //possibilité de rajouter layer
+    {
+        OnGround = true;
+        cst = 1;
+    }
+
+    public void MakeJump()
+    {
+        Vector3 moveVector = new Vector3(0, jumpForce, 0);
+        player.AddForce(moveVector);
+        OnGround = false;
+    }
+    public void AddDirection(int directionIndex) => direction += directions[directionIndex];
+    private void Move(Vector3 direction) => transform.Translate(direction * (speed/cst * Time.deltaTime));
+}
+
+
+
---
+++ Assets/ScriptsWill/TileComponentV2.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileComponentV2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    public Material BlackMaterial;
+    [SerializeField]
+    public Material WhiteMaterial;
+    private ChangeTile test;
+    public MeshRenderer[] ListTiles;
+    Material textureBleu;
+    //private BoxCollider[] ListTiles;
+    void Start()
+    {
+        //MAt = GetComponent<Renderer>().material;
+        //ListTiles = GetComponentsInChildren<BoxCollider>();
+        ListTiles = GetComponentsInChildren<MeshRenderer>();
+
+        //  ListTiles[1].material = textureBleu;
+        var ajds = 1;
+
+        //BlackMaterial = textureBleu;
+        
+
+    }
+
+    //public void OnTriggerEnter(Collider other)
+    //{
+        
+    //}
+    public void OnTriggerEnter()
+
+    {
+        // print("aalalall");
+
+        //if (other.gameObject.layer == 9)
+        //{
+        //Debug.Log("fnjsdnbfjsdfd");
+        //  Destroy(other.gameObject);
+        var text = gameObject.GetComponent<Material>();
+        foreach (MeshRenderer Floor in ListTiles)
+        {
+            //if ((Floor.gameObject.transform.position.x - gameObject.transform.position.x) <= 0.001 &&
+            //    (Floor.gameObject.transform.position.y - gameObject.transform.position.y) <= 0.001)
+            //{
+            Debug.Log("Je passe dans TileComponents");
+            // Floor.material.color = Color.clear;
+            if (Floor.material.color == WhiteMaterial.color)
+            {
+                Floor.material = BlackMaterial;
+            }
+            else { Floor.material = WhiteMaterial; }
+
+
+
+            //Floor.material = Texture;
+
+
+
+        }
+        text = BlackMaterial;
+        var x = gameObject.transform.position.x;
+        var y = gameObject.transform.position.y;
+        // test.Change(x, y);
+        // MAt.color = Color.blue;
+
+        // }
+    }
+    private void OnMouseDown()
+    {
+        foreach (MeshRenderer Floor in ListTiles)
+        {
+            //if ((Floor.gameObject.transform.position.x - gameObject.transform.position.x) <= 0.001 &&
+            //    (Floor.gameObject.transform.position.y - gameObject.transform.position.y) <= 0.001)
+            //{
+            Debug.Log("Je passe dans TileComponents");
+            // Floor.material.color = Color.clear;
+            if (Floor.material.color == WhiteMaterial.color)
+            {
+                Floor.material = BlackMaterial;
+            }
+            else { Floor.material = WhiteMaterial; }
+
+
+
+            //Floor.material = Texture;
+
+
+
+        }
+
+    }
+
+
+
+}
