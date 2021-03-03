a7abdb55c90fad19f83dab71d194d83800d7409b
IzJayy <jeremi.bellemare9@hotmail.com>
2021-03-03 13:44:05 -0500

    Script plateforme mouvement

--- Assets/ScriptsMatthew/PlateformMouvementComponent.cs
+++ Assets/ScriptsMatthew/PlateformMouvementComponent.cs
@@ -4,12 +4,18 @@ using UnityEngine;
 
 public class PlateformMouvementComponent : MonoBehaviour
 {
+    public List<GameObject> Spawns;
+    
+
+
     [SerializeField]
     private float speed = 1.5f;
 
     private bool isMoving = true;
 
+    [SerializeField]
     private Vector3 vector = new Vector3(0, 0, -1);
+
     private Transform Plateform;
     //MovementComponent Follow;
     private void Start()
@@ -23,13 +29,29 @@ public class PlateformMouvementComponent : MonoBehaviour
             Plateform.position += speed * (vector * Time.deltaTime);
     }
 
-    private void OnCollisionEnter(Collision collision)
-    {
-        Debug.Log("ici");
-        var text = GameObject.FindGameObjectWithTag("Player");
-        text.transform.Translate(vector * speed * Time.deltaTime);
+    //private void OnCollisionEnter(Collision collision)
+    //{
+    //    Debug.Log("ici");
+    //    var text = GameObject.FindGameObjectWithTag("Player");
+    //    text.transform.Translate(vector * speed * Time.deltaTime);
 
-        // Follow.Move(vector) = transform.Translate(vector * (speed * Time.deltaTime)); 
+    //    // Follow.Move(vector) = transform.Translate(vector * (speed * Time.deltaTime)); 
+    //}
+    public void DéterminerDirection()
+    {
+        GameObject Spawn1 = GameObject.Find("Spawn1");
+        GameObject Spawn2 = GameObject.Find("Spawn2");
+        GameObject Spawn3 = GameObject.Find("Spawn3");
+
+        Spawns.Add(Spawn1);
+        Spawns.Add(Spawn2);
+        Spawns.Add(Spawn3);
+
+        if (Spawn1.transform.position.z >= 6.77) ;
+
     }
+
+    
+
 
 }
