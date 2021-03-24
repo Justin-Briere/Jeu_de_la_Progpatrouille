0d4212c71526f7e81925bcc94801e8e341894bd1
IzJayy <jeremi.bellemare9@hotmail.com>
2021-03-24 11:27:46 -0400

    Début lvl ventilateur

---
+++ Assets/ScriptsJeremi/MovingFan.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingFan : MonoBehaviour
+{
+    float VitesseRotation = 25.0f;
+   
+    void Update()
+    {
+        transform.Rotate(0, (VitesseRotation * Time.deltaTime), 0);
+    }
+}
---
+++ Assets/ScriptsJeremi/PushForward.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PushForward : MonoBehaviour
+{
+    private Vector3 positionBandit;
+    private Vector3 positionFan;
+    private PushComponent item;
+    BoxCollider i;
+
+    [SerializeField]
+    private float Intensity;
+
+    [SerializeField]
+    float puissanceFan;
+    public void PousserAvant()
+    {
+        item.Push(transform.forward * (Intensity) * 50);
+    }
+    void Start()
+    {
+        item = GetComponent<PushComponent>();
+        i = GetComponentInChildren<BoxCollider>();
+    }
+
+    void Update()
+    {
+        positionBandit = GameObject.Find("Bandit").transform.position;
+        positionFan = GetComponentInParent<Transform>().position;
+        Intensity = 1 / Mathf.Pow(TrouverDistanceBanditFan(positionBandit, positionFan), 2);
+        if (i.isTrigger)
+            PousserAvant();
+    }
+    public float TrouverDistanceBanditFan(Vector3 positionBandit, Vector3 positionFan)
+    {
+
+        return Vector3.Distance(positionBandit, positionFan);
+    }
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    if (other.isTrigger)
+    //        PousserAvant();
+    //}
+}
