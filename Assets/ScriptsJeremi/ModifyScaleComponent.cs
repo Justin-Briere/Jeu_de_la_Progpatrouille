3a8a2b6d9c99edb28a3cc4bc874dd6a5dc23e6a9
IzJayy <jeremi.bellemare9@hotmail.com>
2021-03-25 09:21:07 -0400

    Menu Bouton

---
+++ Assets/ScriptsJeremi/ModifyScaleComponent.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModifyScaleComponent : MonoBehaviour    // Script inspiré de la session passée
+{
+    Vector3 Grandeur = new Vector3(1, 1, 1);
+
+    public void AgrandirBouton()
+    {
+        transform.localScale = new Vector3(1.5f, 1.5f, 1.5f);
+    }
+
+    public void RétrécirBouton()
+    {
+        transform.localScale = Vector3.one;
+    }
+}
+
---
+++ Assets/ScriptsJeremi/QuitterApplicationComponent.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitterApplicationComponent : MonoBehaviour
+{
+    public void Quitter()  //Script dernière session
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+
+#else
+            Application.Quit();
+#endif
+    }
+
+}
+//public void QuitGame()
+//    {
+//        Application.Quit();
+//        Debug.Log("Game is exiting");
+//    }
+//}
+
