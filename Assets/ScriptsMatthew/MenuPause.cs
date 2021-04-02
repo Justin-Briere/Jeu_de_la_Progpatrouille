337432b29593ac9f92ad917ae018392c2e8b71f9
MatthewVerret <201973658@edu.clg.qc.ca>
2021-04-02 12:27:54 -0400

    Pas finit = Menu escape

---
+++ Assets/ScriptsMatthew/MenuPause.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuPause : MonoBehaviour
+{
+    public static bool IsJeuxArret = false;
+
+    public GameObject arretMenu;
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (IsJeuxArret)
+                Restart();
+            else
+                StopGame();
+        }
+    }
+    private void Restart()
+    {
+        arretMenu.SetActive(false);
+        Time.timeScale = 1f;
+        IsJeuxArret = false;
+    }
+    private void StopGame()
+    {
+        arretMenu.SetActive(true);
+        Time.timeScale = 0f;
+        IsJeuxArret = true;
+    }
+    private void QuitterPartie()
+    {
+        Application.Quit();
+    }
+
+}
