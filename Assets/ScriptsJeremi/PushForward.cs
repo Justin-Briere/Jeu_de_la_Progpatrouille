96bb1636b3b67b72158a3dc75acb5f027cd89684
IzJayy <jeremi.bellemare9@hotmail.com>
2021-04-04 22:32:19 -0400

    niveau ventilateur concept

--- Assets/ScriptsJeremi/PushForward.cs
+++ Assets/ScriptsJeremi/PushForward.cs
@@ -6,40 +6,62 @@ public class PushForward : MonoBehaviour
 {
     private Vector3 positionJoueur;
     private Vector3 positionFan;
+    private GameObject joueur;
     private PushComponent item;
     BoxCollider i;
+    private bool vérif;
 
     [SerializeField]
+    int zePuissance = 7;
+
+    //[SerializeField]
     private float Intensity;
 
     [SerializeField]
     float puissanceFan;
     public void PousserAvant()
     {
-        item.Push(transform.forward * (Intensity) * 50);
+        //joueur.transform.(transform.forward * (Intensity) * 50);
+        joueur.GetComponent<Rigidbody>().velocity = transform.forward * Intensity * zePuissance;
     }
     void Start()
     {
+        joueur = GameObject.Find("LeJoueur");
         item = GetComponent<PushComponent>();
         i = GetComponentInChildren<BoxCollider>();
     }
 
     void Update()
     {
-        positionJoueur = GameObject.Find("LeJoueur").transform.position;
-        positionFan = GetComponentInParent<Transform>().position;
-        Intensity = 1 / Mathf.Pow(TrouverDistanceBanditFan(positionJoueur, positionFan), 2);
-        if (i.isTrigger)
+
+        if (vérif)
+        {
+            positionJoueur = joueur.transform.position;
+            positionFan = GetComponentInParent<Transform>().position;
+            Intensity = Mathf.Pow(Mathf.Log(TrouverDistanceBanditFan(positionJoueur, positionFan)), -1);
             PousserAvant();
+        }
+
     }
     public float TrouverDistanceBanditFan(Vector3 positionBandit, Vector3 positionFan)
     {
 
         return Vector3.Distance(positionBandit, positionFan);
     }
-    //private void OnTriggerEnter(Collider other)
-    //{
-    //    if (other.isTrigger)
-    //        PousserAvant();
-    //}
+    private void OnTriggerEnter(Collider other)
+    {
+        //if (other.isTrigger)
+
+        //positionJoueur = joueur.transform.position;
+        //positionFan = GetComponentInParent<Transform>().position;
+        //Intensity = Mathf.Pow(Mathf.Log(TrouverDistanceBanditFan(positionJoueur, positionFan)), -1);
+        //PousserAvant();
+        vérif = true;
+        
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        joueur.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        vérif = false;
+    }
 }
