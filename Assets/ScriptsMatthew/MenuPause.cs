96f24bb73d58eddd2e2e0853735f4945920bba0e
MatthewVerret <201973658@edu.clg.qc.ca>
2021-04-12 11:13:12 -0400

    menu game

---
+++ Assets/ScriptsMatthew/MenuPause.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuPause : MonoBehaviour
+{
+    public static bool IsJeuxArret = false;
+
+    public GameObject arretMenu;
+    void Start()
+    {
+        arretMenu.SetActive(false);
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (IsJeuxArret)
+                Restart();
+            else
+                StopGame();
+        }
+    }
+    public void Restart()
+    {
+        arretMenu.SetActive(false);
+        Time.timeScale = 1f;
+        IsJeuxArret = false;
+    }
+    public void StopGame()
+    {
+        arretMenu.SetActive(true);
+        Time.timeScale = 0f;
+        IsJeuxArret = true;
+    }
+    public void QuitterPartie()
+    {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+    }
+
+}
---
+++ Assets/ScriptsMatthew/ToujoursPresent.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToujoursPresent : MonoBehaviour
+{
+    private static bool IsCreated;
+
+    static ToujoursPresent()
+    {
+        IsCreated = false;
+    }
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if(!IsCreated)
+        {
+            DontDestroyOnLoad(this.gameObject);
+            IsCreated = true;
+        }
+    }
+}
