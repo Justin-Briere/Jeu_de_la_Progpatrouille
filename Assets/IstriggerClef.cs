9c0f50d31198a0b37070e265eb70736b2834f7c5
HEADSHOT739 <justin.briere1@gmail.com>
2021-04-14 11:58:25 -0400

    labyrinthe update

---
+++ Assets/IstriggerClef.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IstriggerClef : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private void OnTriggerEnter(Collider other) => Destroy(this.gameObject);
+      
+   
+    
+}
---
+++ Assets/showImageScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class showImageScript : MonoBehaviour
+{
+    RawImage Image;
+    bool once = true;
+    TimeManager verif;
+    private void Start()
+    {
+        Image = GetComponent<RawImage>();
+    }
+
+    // Update is called once per frame
+    void Update()
+
+    {
+        
+        if (TryGetComponent<GameObject>(out GameObject clef)) Image.enabled = true ;
+            
+
+    }
+    private IEnumerable Wait()
+    {
+        if (once)
+        {
+            once = false;
+            yield return new WaitForSeconds(10);
+        }
+    }
+}
