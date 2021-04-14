98044e5b760f5c981d11d43d9da352d660507709
WillMart91 <WillMartin91@outlook.com>
2021-04-14 10:28:26 -0400

    piece AI fini
    Manque juste les policier qui tue

--- Assets/ScriptsWill/ChasePolice.cs
+++ Assets/ScriptsWill/ChasePolice.cs
@@ -45,13 +45,13 @@ public class ChasePolice : MonoBehaviour //ce script s'occupe de poursuivre le p
             float positionBanditZ = GameObject.Find("Bandit").transform.position.z;
             policeSpeed = 7;
             ChaseBandit(positionPoliceX, positionPoliceZ, positionBanditX, positionBanditZ);
-            chasing = true;
+            //chasing = true;
         }
         else
         {
             policeSpeed = 2;
             ReturnInitialPosition(positionInitialeX, positionInitialeZ, positionPoliceX, positionPoliceZ);
-            chasing = false;
+            //chasing = false;
 
         }
     }
@@ -74,7 +74,7 @@ public class ChasePolice : MonoBehaviour //ce script s'occupe de poursuivre le p
         float xDiff = (positionInitialeX - positionPoliceX);
         float zDiff = (positionInitialeZ - positionPoliceZ);
 
-        if (Mathf.Abs(xDiff) <= 0.1 && Mathf.Abs(zDiff) <= 0.1)
+        if (Mathf.Abs(xDiff) <= 0.4 && Mathf.Abs(zDiff) <= 0.4)
         {
             current++;
 
@@ -88,9 +88,11 @@ public class ChasePolice : MonoBehaviour //ce script s'occupe de poursuivre le p
         }
         else
         {
-
-            Deplacer(xDiff, zDiff);
-            transform.LookAt(allo[current]);
+            if (allo.Length != 1)
+            {
+                Deplacer(xDiff, zDiff);
+                transform.LookAt(allo[current]);
+            }
         }
 
         //if (positionPoliceX - allo[current].position.x <= 0.5 && positionPoliceZ - allo[current].position.z <= 0.5)
