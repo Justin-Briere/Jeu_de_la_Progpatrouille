c56eab77800b8c5ac59af845502d89f040d7ddab
WillMart91 <WillMartin91@outlook.com>
2021-04-27 21:56:01 -0400

    choose difficulty attempt

---
+++ Assets/ScriptsWill/ChooseDifficulty.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChooseDifficulty : MonoBehaviour
+{
+    int difficulty;
+
+    private void OnCollisionEnter(Collision porte)
+    {
+        print("efweffwfew");
+        if (porte.gameObject.tag == "easy")
+        {
+            difficulty = 1;
+            print("diff choisie est ez");
+        }
+            
+        if (porte.gameObject.tag == "Medium")
+            difficulty = 2;
+        if (porte.gameObject.tag == "Hard")
+            difficulty = 3;
+    }
+}
