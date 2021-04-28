9a168c9ffa7aa3efe36a311944c6e38ece684569
WillMart91 <WillMartin91@outlook.com>
2021-04-28 10:53:52 -0400

    labyrinthe complet
    reste lumiere et visuels

--- Assets/script Justin/createLab.cs
+++ Assets/script Justin/createLab.cs
@@ -29,11 +29,25 @@ public class createLab : MonoBehaviour
         {
             for (int j = 0; j < Map.GetLength(1); ++j)
             {
-                var test = Map[j, i] == '*' ? Instantiate(wall, new Vector3(2*j,0,2*i), new Quaternion(0,0,0,0)) : null;
+                var test = Map[j, i] == '*' ? Instantiate(wall, new Vector3(wall.transform.localScale.x*j,0, wall.transform.localScale.y* i), new Quaternion(0,0,0,0)) : null;
 
             }
         }
+
+
+        for (int j = 1; j < Map.GetLength(1); ++j)
+              Instantiate(wall, new Vector3(wall.transform.localScale.x * j, 0, 0), new Quaternion(0, 0, 0, 0));
+
+
+        for (int i = 0; i < Map.GetLength(0); ++i)
+            Instantiate(wall, new Vector3(0, 0, wall.transform.localScale.x * i), new Quaternion(0, 0, 0, 0));
+
+
+
+
     }
+
+
 
    
 }
