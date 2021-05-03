13ff7344484b0e9ea38aec6ce0003c09b58a4f00
MatthewVerret <201973658@edu.clg.qc.ca>
2021-05-03 12:07:54 -0400

    Musique
    Regarder pourquoi 2 audio listener quand je suis dans la final scene

---
+++ Assets/RestartGame.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartGame : MonoBehaviour
+{
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.P))
+        {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+        }
+    }
+}
