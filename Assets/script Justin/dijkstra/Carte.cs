57db280bb77895bec6b179c865cd6f74326b6d71
WillMart91 <WillMartin91@outlook.com>
2021-05-05 09:53:04 -0400

    Update Carte.cs

--- Assets/script Justin/dijkstra/Carte.cs
+++ Assets/script Justin/dijkstra/Carte.cs
@@ -5,84 +5,104 @@ using System.Linq;
 using System.Collections.Generic;
 using System.IO;
 
-public class Carte 
+public class Carte
 {
     //"../../../Cartes/";
-    const string CheminAccès = "/.../.../.../../.../Jeu_de_la_Progpatrouille/Assets/script Justin/dijkstra/Cartes/"; 
-        const char Source = 'S';
-        const char Destination = 'D';
-        const char Accessible = '.';
-        const char Inaccessible = '*';
-        public int LargeurCarte { get; private set; }
-        public int HauteurCarte { get; private set; }
+    const string CheminAccès = "/.../.../.../../.../Jeu_de_la_Progpatrouille/Assets/script Justin/dijkstra/Cartes/";
+    const char Source = 'S';
+    const char Destination = 'D';
+    const char Accessible = '.';
+    const char Inaccessible = '*';
+    public int LargeurCarte { get; private set; }
+    public int HauteurCarte { get; private set; }
 
-        public Point2D PositionSource { get; private set; }
-        public Point2D PositionDestination { get; private set; }
+    public Point2D PositionSource { get; private set; }
+    public Point2D PositionDestination { get; private set; }
 
-        public bool[,] DataCarte;
+    public bool[,] DataCarte;
 
-        public bool this[int y, int x]
+    public bool this[int y, int x]
+    {
+        get
         {
-            get
-            {
-                return DataCarte[y, x];
-            }
-        }
-
-        public Carte(string nomFichier)
-        {
-
-            PositionSource = new Point2D(-1, -1);
-            PositionDestination = new Point2D(-1, -1);
-            string[] donnéesBrutesCarte = LireDonnéesCarte(CheminAccès + nomFichier);
-            DataCarte = CréerCarte(donnéesBrutesCarte);
-        }
-
-        static string[] LireDonnéesCarte(string nomFichier)
-        {
-            char[] séparateurs = new char[] { '\t' };
-            List<string> listeDonnéeCarte = new List<string>();
-            StreamReader fEntrée = new StreamReader(nomFichier);
-            while (!fEntrée.EndOfStream)
-            {
-                string ligneCarte = fEntrée.ReadLine();
-                listeDonnéeCarte.Add(ligneCarte);
-            }
-            fEntrée.Close();
-            return listeDonnéeCarte.ToArray();
-        }
-
-        bool[,] CréerCarte(string[] donnéesBrutesCarte)
-        {
-
-            LargeurCarte = donnéesBrutesCarte.Min(x => x.Length);
-            HauteurCarte = donnéesBrutesCarte.Length;
-            bool[,] modèleCarte = new bool[HauteurCarte, LargeurCarte];
-            for (int y = 0; y < HauteurCarte; ++y)
-            {
-                for (int x = 0; x < LargeurCarte; ++x)
-                {
-                    char symbole = donnéesBrutesCarte[y][x];
-                    modèleCarte[y, x] = TraiterDonnéeBrute(symbole, x, y);
-                }
-            }
-            return modèleCarte;
-        }
-
-        private bool TraiterDonnéeBrute(char symbole, int x, int y)
-        {
-            if (symbole == Source)
-            {
-                PositionSource = new Point2D(x, y);
-            }
-            else
-            {
-                if (symbole == Destination)
-                {
-                    PositionDestination = new Point2D(x, y);
-                }
-            }
-            return symbole != Inaccessible;
+            return DataCarte[y, x];
         }
     }
+
+    public Carte(string nomFichier)
+    {
+       // idk();
+
+
+
+
+
+
+
+        PositionSource = new Point2D(-1, -1);
+        PositionDestination = new Point2D(-1, -1);
+        string[] donnéesBrutesCarte = LireDonnéesCarte(CheminAccès + nomFichier);
+        DataCarte = CréerCarte(donnéesBrutesCarte);
+    }
+
+    static string[] LireDonnéesCarte(string nomFichier)
+    {
+        char[] séparateurs = new char[] { '\t' };
+        List<string> listeDonnéeCarte = new List<string>();
+        StreamReader fEntrée = new StreamReader(nomFichier);
+        while (!fEntrée.EndOfStream)
+        {
+            string ligneCarte = fEntrée.ReadLine();
+            listeDonnéeCarte.Add(ligneCarte);
+        }
+        fEntrée.Close();
+        return listeDonnéeCarte.ToArray();
+    }
+
+    bool[,] CréerCarte(string[] donnéesBrutesCarte)
+    {
+
+        LargeurCarte = donnéesBrutesCarte.Min(x => x.Length);
+        HauteurCarte = donnéesBrutesCarte.Length;
+        bool[,] modèleCarte = new bool[HauteurCarte, LargeurCarte];
+        for (int y = 0; y < HauteurCarte; ++y)
+        {
+            for (int x = 0; x < LargeurCarte; ++x)
+            {
+                char symbole = donnéesBrutesCarte[y][x];
+                modèleCarte[y, x] = TraiterDonnéeBrute(symbole, x, y);
+            }
+        }
+        return modèleCarte;
+    }
+
+    private bool TraiterDonnéeBrute(char symbole, int x, int y)
+    {
+        if (symbole == Source)
+        {
+            PositionSource = new Point2D(x, y);
+        }
+        else
+        {
+            if (symbole == Destination)
+            {
+                PositionDestination = new Point2D(x, y);
+            }
+        }
+        return symbole != Inaccessible;
+    }
+
+    //public string idk()
+    //{
+    //    string[] res = System.IO.Directory.GetFiles(Application.dataPath, "YourEitorScript.cs", SearchOption.AllDirectories);
+    //    if (res.Length == 0)
+    //    {
+    //        Debug.LogError("error message ....");
+    //        return null;
+    //    }
+    //    string path = res[0].Replace("HierarchyCustomiserEd.cs", "").Replace("\\", "/");
+    //    Debug.Log(path);
+    //    return path;
+    //}
+}
 
