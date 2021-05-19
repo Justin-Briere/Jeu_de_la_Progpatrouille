2fc9e56813657fa139fe5ab38df96ed4f7b3b690
IzJayy <jeremi.bellemare9@hotmail.com>
2021-05-19 12:58:14 -0400

    ménage

--- Assets/ScriptsJeremi/Tore.cs
+++ Assets/ScriptsJeremi/Tore.cs
@@ -3,7 +3,7 @@ using System;
 using System.Collections.Generic;
 using UnityEngine;
 
-public sealed class Tore : MonoBehaviour
+public sealed class Tore : MonoBehaviour  //Script de la dernière session.
 {
     const int TILES_TRIANGLE = 2;
     const int APEX_TRIANGLE = 3;
