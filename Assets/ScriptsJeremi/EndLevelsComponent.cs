1a4aa82bfe686b594c0d24a2f4440467cdf26179
IzJayy <jeremi.bellemare9@hotmail.com>
2021-05-21 09:48:46 -0400

    push final

--- Assets/ScriptsJeremi/EndLevelsComponent.cs
+++ Assets/ScriptsJeremi/EndLevelsComponent.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class EndLevelsComponent : MonoBehaviour
+public class EndLevelsComponent : MonoBehaviour //Classe permettant de faire les changements de scène après les niveaux.
 {
     string sceneName;
     void Start()
