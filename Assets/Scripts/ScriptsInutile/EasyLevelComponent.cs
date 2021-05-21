2004bcb3bb805f98c0d3a8089cf199344b51e9bf
IzJayy <jeremi.bellemare9@hotmail.com>
2021-05-20 21:04:24 -0400

    changement scène piège

--- Assets/Scripts/ScriptsInutile/EasyLevelComponent.cs
+++ Assets/Scripts/ScriptsInutile/EasyLevelComponent.cs
@@ -5,26 +5,26 @@ using UnityEngine.SceneManagement;
 
 public class EasyLevelComponent : MonoBehaviour
 {
-    public bool DifficultéFacile = false;
-    public bool DifficultéIntermédiaire = false;
-    public bool DifficultéDifficile = false;
-    [SerializeField]
-    Scene next;
-    private void DéterminerDifficultéJeu()
-    {
-        if (gameObject.layer == 13)
-            DifficultéFacile = true;
-        else if (gameObject.layer == 14)
-            DifficultéIntermédiaire = true;
-        else if (gameObject.layer == 15)
-            DifficultéDifficile = true;
-    }
+    //public bool DifficultéFacile = false;
+    //public bool DifficultéIntermédiaire = false;
+    //public bool DifficultéDifficile = false;
+    //[SerializeField]
+    //Scene next;
+    //private void DéterminerDifficultéJeu()
+    //{
+    //    if (gameObject.layer == 13)
+    //        DifficultéFacile = true;
+    //    else if (gameObject.layer == 14)
+    //        DifficultéIntermédiaire = true;
+    //    else if (gameObject.layer == 15)
+    //        DifficultéDifficile = true;
+    //}
 
     private void OnCollisionEnter(Collision collision)
     {
         
            
-            Debug.Log("Collision Detected");
+            //Debug.Log("Collision Detected");
             SceneManager.LoadScene("TESTVAISSEAU");
             
             
